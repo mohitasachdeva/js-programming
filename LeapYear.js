de44e749aa771bbe747c4a0d9415abd7b87b2f41
HP <sachdevamohita5@gmail.com>
2022-08-18 23:10:21 +0530

    check leap year

---
+++ LeapYear.js
@@ -0,0 +1,18 @@
+function checkYear() {
+      var year=2000;
+        if (year % 400 == 0)
+            console.log("it is a leap year")
+         else if (year % 100 == 0)
+            console.log("it is not a leap year")
+ 
+      
+        else if (year % 4 == 0)
+            console.log("it a leap year")
+            else
+            console.log( " year ;is not a leap year")
+        
+    }
+ checkYear();
+   
+      
+       
